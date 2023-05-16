3b687089d4b60ad53492da41b9dfa36b94fc3971
AhmedAshrafAZ <Ahmed-276@protonmail.com>
2023-05-16 11:05:52 +0300

    📦 NEW: add basic layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useThemesStore } from '@/store/themesStore';
+const { currentTheme } = storeToRefs(useThemesStore());
+</script>
+
+
+<template>
+  <div :data-theme="currentTheme" class="p-2 h-screen w-full flex flex-row gap-2">
+    <slot />
+  </div>
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Color from '@/types/Color';
+import { storeToRefs } from 'pinia';
+import AshButton from '@/components/AshButton.vue';
+import { useThemesStore } from '@/store/themesStore';
+const { getAllThemes, setTheme } = useThemesStore();
+const { currentTheme } = storeToRefs(useThemesStore());
+onMounted(() => {
+    setTheme(getAllThemes[0]);
+});
+</script>
+
+
+<template >
+    <div class="bg-base-100 w-1/6 h-full rounded-lg border-2 p-4 overflow-y-scroll flex flex-col">
+        <button
+            :class="`${currentTheme === theme ? 'bg-primary text-primary-content' : 'bg-base-100 text-base-content'} p-2 m-2 rounded-xl`"
+            @click="setTheme(theme)" v-for="theme in getAllThemes">{{ theme }}</button>
+    </div>
+</template>
