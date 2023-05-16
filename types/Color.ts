cf7b3b19d40df35e0da148260e94b4e28034fcda
AhmedAshrafAZ <Ahmed-276@protonmail.com>
2023-05-16 11:07:33 +0300

    📦 NEW: add types

---
+++ types/Color.ts
@@ -0,0 +1,13 @@
+enum Color {
+    primary = "primary",
+    secondary = "secondary",
+    accent = "accent",
+    neutral = "neutral",
+    base = "base",
+    info = "info",
+    success = "success",
+    warning = "warning",
+    error = "error"
+}
+
+export default Color
