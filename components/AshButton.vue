2c64c3cd7db50d680a64833984edb18d668d66e7
AhmedAshrafAZ <Ahmed-276@protonmail.com>
2023-05-20 04:45:50 +0300

    📦 NEW: add custom components

---
+++ components/AshButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useThemesStore } from '@/store/themesStore';
+
+import Color from '@/types/Color';
+defineProps(
+  {
+    color: {
+      type: String as PropType<Color>,
+      required: true,
+    },
+  }
+)
+
+const { setColor } = useThemesStore();
+const capitalizeColor = (color: string) => {
+  return color.charAt(0).toUpperCase() + color.slice(1);
+}
+
+</script>
+
+
+<template>
+  <button @click="setColor(color)"
+    :class="`hover:bg-${color}-focus bg-${color} text-${color}-content py-3 px-6 rounded-md`">
+    {{ capitalizeColor(color) }}
+  </button>
+</template>
+
---
+++ components/AshInput.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useThemesStore } from '@/store/themesStore';
+
+const { currentColor } = storeToRefs(useThemesStore());
+let message = ref('');
+
+</script>
+
+
+<template>
+  <div class="flex flex-row gap-4 w-full">
+    <textarea v-model="message" placeholder="Type something here..."
+      :class="`resize-none w-1/2 p-3 overflow-hidden rounded-lg outline-none border-2 border-dotted focus:border-solid focus:border-${currentColor} bg-base-darker hover:bg-base-darkest focus:bg-base-darkest text-base-content`"></textarea>
+    <p
+      :class="`w-1/2 text-justify hover:bg-${currentColor}-focus bg-${currentColor} text-${currentColor}-content p-3 rounded-md`">
+      {{ message.trim() === '' ? 'Type something in the input box' : message }}
+    </p>
+  </div>
+</template>
+
