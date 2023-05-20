d3480de5fd0c98845bba7265b52041cc5415b23b
AhmedAshrafAZ <Ahmed-276@protonmail.com>
2023-05-20 04:47:20 +0300

    📦 NEW: add theme colors file

---
+++ assets/tailwind/themes.ts
@@ -0,0 +1,40 @@
+import type { Theme } from "@/types/Theme"
+
+const dark: Theme = {
+    "--color-primary": "hsl(262.35, 80.315%, 50.196%)",
+    "--color-primary-content": "hsl(0, 0%, 100%)",
+    "--color-primary-focus": "hsl(262.35, 80.315%, 40.157%)",
+
+    "--color-secondary": "hsl(315.75, 70.196%, 50%)",
+    "--color-secondary-content": "hsl(0, 0%, 100%)",
+    "--color-secondary-focus": "hsl(315.75, 70.196%, 40%)",
+
+    "--color-accent": "hsl(174.69, 70.335%, 40.98%)",
+    "--color-accent-content": "hsl(0, 0%, 100%)",
+    "--color-accent-focus": "hsl(174.69, 70.335%, 32.784%)",
+
+    "--color-neutral": "hsl(218.18, 18.033%, 11.961%)",
+    "--color-neutral-content": "hsl(220, 13.376%, 69.216%)",
+    "--color-neutral-focus": "hsl(222.86, 17.073%, 8.0392%)",
+
+    "--color-base": "hsl(220, 17.647%, 20%)",
+    "--color-base-content": "hsl(220, 13.376%, 69.216%)",
+    "--color-base-darker": "hsl(220, 17.241%, 17.059%)",
+    "--color-base-darkest": "hsl(218.57, 17.949%, 15.294%)",
+
+    "--color-info": "hsl(198, 93%, 60%)",
+    "--color-info-content": "hsl(198, 100%, 12%)",
+
+    "--color-success": "hsl(158, 64%, 52%)",
+    "--color-success-content": "hsl(158, 100%, 10%)",
+
+    "--color-warning": "hsl(43, 96%, 56%)",
+    "--color-warning-content": "hsl(43, 100%, 11%)",
+
+    "--color-error": "hsl(0, 91%, 71%)",
+    "--color-error-content": "hsl(0, 100%, 14%)",
+}
+
+export default {
+    dark,
+}
