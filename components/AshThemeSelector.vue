12f6479b88e0737ccd36de572fa628120c1fa402
AhmedAshrafAZ <Ahmed-276@protonmail.com>
2023-05-21 07:30:20 +0300

    📦 NEW: add theme selector for small devices

---
+++ components/AshThemeSelector.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useThemesStore } from '@/store/themesStore';
+
+const { currentTheme, getAllThemes } = storeToRefs(useThemesStore());
+const { setTheme } = useThemesStore();
+
+</script>
+
+
+<template>
+  <select @change="setTheme($event.target!.value as typeof currentTheme.value)"
+    class="overflow-hidden p-3 h-14  bg-base-darker hover:bg-base-darkest text-base-content rounded-md outline-none border-2 border-dotted hover:border-solid focus:border-solid">
+    <option v-for="theme in getAllThemes" :value="theme" :selected="theme === currentTheme">{{ theme }}
+    </option>
+  </select>
+</template>
+
