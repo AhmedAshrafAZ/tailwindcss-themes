8360091a439245b97d9c5e3a0d782eae3f6f0494
AhmedAshrafAZ <Ahmed-276@protonmail.com>
2023-05-21 07:30:55 +0300

    🐛 FIX: responsiveness

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import Color from '@/types/Color';
 import { storeToRefs } from 'pinia';
-import AshButton from '@/components/AshButton.vue';
-import AshInput from '@/components/AshInput.vue';
 import { useThemesStore } from '@/store/themesStore';
 const { getAllThemes, setTheme } = useThemesStore();
 const { currentTheme } = storeToRefs(useThemesStore());
@@ -13,18 +11,26 @@ onMounted(() => {
 
 
 <template >
-    <div class="bg-base-darker w-1/6 h-full rounded-lg border-2 border-base-darkest p-4 overflow-y-scroll flex flex-col">
+    <div
+        class="bg-base-darker w-1/6 h-full rounded-lg border-2 border-base-darkest p-4 overflow-y-scroll flex flex-col collapse lg:visible">
         <button
             :class="`${currentTheme === theme ? 'bg-primary text-primary-content' : 'bg-base-100 text-base-content'} p-2 m-2 rounded-xl`"
             @click="setTheme(theme as typeof currentTheme.value)" v-for="theme in getAllThemes">{{ theme }}</button>
     </div>
-    <div class="bg-base w-5/6 h-full rounded-lg border-2 border-base-darkest p-4 overflow-hidden">
-        <div class="flex flex-row gap-6 justify-between mb-4">
+
+    <div
+        class="bg-base-darker w-full rounded-lg border-2 border-base-darkest p-4 overflow-y-scroll flex flex-col lg:collapse visible">
+        <AshThemeSelector />
+    </div>
+    <div class="bg-base w-full lg:w-5/6 h-full rounded-lg border-2 border-base-darkest p-4 overflow-hidden">
+
+        <div class="mb-4 w-full">
+            <AshInput />
+        </div>
+
+        <div class="flex flex-row flex-wrap  gap-6 justify-evenly my-8">
             <AshButton v-for="(c) in Object.keys(Color)" :color="(c as Color)" />
         </div>
 
-        <div class="my-8 w-full">
-            <AshInput />
-        </div>
     </div>
 </template>
